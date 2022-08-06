566b43b366b06d55a1953c26456e0b378c5281ba
daeunkwak <misodaeun3@naver.com>
2022-08-07 01:00:46 +0900

    Initial commit

---
+++ src/test/java/daeunn/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package daeunn.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
